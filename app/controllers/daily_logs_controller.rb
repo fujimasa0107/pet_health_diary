31e1041ba9729b236b21f0a96b5f8ff8be5bd87c
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-03-05 15:03:10 +0900

    コントローラー追加

--- app/controllers/daily_logs_controller.rb
+++ app/controllers/daily_logs_controller.rb
@@ -1,9 +1,26 @@
 class DailyLogsController < ApplicationController
 
   def index
+
   end
 
   def new
+    @daily_log = DailyLog.new
+  end
+
+  def create
+    
+    @daily_log = DailyLog.new(daily_log_params)
+    if @daily_log.save
+      redirect_to daily_logs_path, notice: '記録が作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+  def daily_log_params
+    params.require(:daily_log).permit(:pet_id, :date, :food_intake, :water_intake, :toilet_count, :exercise_time, :image).merge(user_id: current_user.id)
   end
 
 end
