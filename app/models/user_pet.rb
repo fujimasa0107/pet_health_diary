79248a5dae97fe840897cf4487dc307069e9cfa0
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-03-10 22:29:32 +0900

    モデル修正、updateでメール、パスワードをなくてもできるように

--- app/models/user_pet.rb
+++ app/models/user_pet.rb
@@ -1,10 +1,10 @@
 class UserPet
   include ActiveModel::Model
-  attr_accessor :email, :password, :password_confirmation, :name, :pet_name, :pet_age, :pet_weight
+  attr_accessor :email, :password, :password_confirmation, :name, :pet_name, :pet_age, :pet_weight, :update_context, :user
 
-  validates :email, presence: { message: 'Please enter an email address' }
-  validates :password, presence: { message: 'Please enter a password' }
-  validates :password_confirmation, presence: { message: 'Please confirm your password' }
+  validates :email, presence: { message: 'Please enter an email address' }, unless: -> { update_context }
+  validates :password, presence: { message: 'Please enter a password' }, confirmation: true, unless: -> { update_context }
+  validates :password_confirmation, presence: { message: 'Please confirm your password' }, unless: -> { update_context }
   validates :name, presence: { message: 'Please enter a name' }
   validates :pet_name, presence: { message: 'Please enter your pet\'s name' }
   validates :pet_age, numericality: { only_integer: true, greater_than: 0, message: 'Please enter a valid pet age' }
@@ -13,13 +13,23 @@ class UserPet
   def save
     return false unless valid?
 
-    user = User.new(email: email, password: password, password_confirmation: password_confirmation, name: name)
-    if user.save
-      user.pets.create(name: pet_name, age: pet_age, weight: pet_weight)
-      return user
+    if update_context
+      # 更新処理
+      #user = User.find_by(email: email) # または current_user を使用
+      user.update(name: name)
+      pet = user.pets.first # 仮定: ユーザーには少なくとも1つのペットが関連付けられている
+      pet.update(name: pet_name, age: pet_age, weight: pet_weight)
+      true
     else
-      user.errors.full_messages.each { |msg| errors.add(:base, msg) }
-      return false
+      # 新規作成処理
+      user = User.new(email: email, password: password, password_confirmation: password_confirmation, name: name)
+      if user.save
+        user.pets.create(name: pet_name, age: pet_age, weight: pet_weight)
+        return user
+      else
+        user.errors.full_messages.each { |msg| errors.add(:base, msg) }
+        return false
+      end
     end
   end
 end
