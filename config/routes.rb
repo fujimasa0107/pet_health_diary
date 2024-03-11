e3e0e7254d630d7656f1b185b87796ea50a7529a
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-03-12 08:21:55 +0900

    記事ルーティング追加、リンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
     resources :pets, only: [:new, :create, :edit , :update]
   end
 
+  #管理に関するルーティング
   resources :daily_logs
+
+  # 記事に関するルーティング
+  resources :articles, only: [:index, :new]  # 全ての記事を表示するためのルーティング
+  resources :categories do
+    resources :articles, only: [:index, :new] # 特定のカテゴリーの記事を表示するためのルーティング
+  end
 end
