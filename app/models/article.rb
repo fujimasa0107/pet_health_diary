62cf94025344c09937b5e813d516af7e46ff1011
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-03-12 14:09:04 +0900

    db、モデル追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: 20 } # タイトルは必須かつ20文字以内
+  validates :content, presence: true # コンテンツは必須
 end
