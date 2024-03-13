d528633d4cdd6b1a7c178c7b023922458ea519a1
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-03-13 12:40:48 +0900

    コントローラー

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    # 月間ランキング用の記事
     @ranking_articles = Article.where('created_at >= ?', 1.month.ago)
                                .order(views_count: :desc)
-                               .limit(10) # トップ10の記事
+                               .limit(10)
+  
+    # カテゴリIDがパラメータとして存在する場合、そのカテゴリに属する記事を取得
+    if params[:category_id].present?
+      @articles = Article.where(category_id: params[:category_id])
+    else
+      @articles = Article.all
+    end
   end
 
   def new
@@ -26,7 +33,12 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:category_id, :title, :content, :image)
+    params.require(:article).permit(
+      :category_id,
+      :title,
+      :content,
+      :image
+    ).merge(user_id: current_user.id)
   end
 end
 
