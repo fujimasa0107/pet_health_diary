d19185d18b1e8fb4059b4892eb8b836d68de9ae3
fujimasa0107 <158474627+fujimasa0107@users.noreply.github.com>
2024-03-13 12:43:35 +0900

    js 準備　修正

--- app/javascript/revert_check.js
+++ app/javascript/revert_check.js
@@ -1,27 +1,19 @@
-document.addEventListener('turbo:load', function() {
-  const checkButtons = document.querySelectorAll('.article-check-button');
-  checkButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      const articleId = this.getAttribute('data-id');
-
-        if (confirm('既読を未読に戻しますか？')) {
-      fetch(`/checks/${articleId}/revert_check`, {
-        method: 'POST',
-        headers: {
-          'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id: articleId })
-      })
-      .then(response => {
-        if (response.ok) {
-          this.remove();
-        } else {
-          console.error('Error:', response);
+window.addEventListener('turbo:load', function(){
+  const articleCheckButtons = document.querySelectorAll(".article-check-button");
+  articleCheckButtons.forEach(function (button) {
+    button.addEventListener("click", () => {
+      const deleteFlg = window.confirm('記事の既読を削除しますか？');
+      if(deleteFlg) {
+        const articleId = button.getAttribute("data");
+        const XHR = new XMLHttpRequest();
+        XHR.open("GET", `/checks/revert/${articleId}`, true);
+        XHR.send();
+        XHR.onload = () => {
+          button.remove();
         }
-      })
-      .catch(error => console.error('Error:', error));
-     }
+      } else {
+        ;
+      }
     });
   });
 });
